b10fcbc89fee07f6f43059e33ce49b560b72a305
Stephen Wissow <stephen.wissow@unh.edu>
2022-02-24 21:13:50 -0500

    Add preamble and tweak header

--- supreme-enigma.sh
+++ supreme-enigma.sh
@@ -4,17 +4,18 @@ TIMESTAMP=$(date +'%Y-%m-%d-%H%M-%S')
 OUTFILE=supreme-enigma-$TIMESTAMP.txt
 MESSAGE=$*
 
-echo "Collecting network data at $TIMESTAMP"
-echo "Message for this collection: \"$MESSAGE\""
-echo "Saving data to: $OUTFILE"
+echo "Collecting network data at $TIMESTAMP" | tee -a "$OUTFILE"
+echo "Message for this collection: \"$MESSAGE\"" | tee -a "$OUTFILE"
 
 declare -a COMMANDS=("cat /etc/resolv.conf" "resolvectl status")
 
 for command in "${COMMANDS[@]}"
 do
     {
-        printf "\n********** BEGIN %s **********\n" "$command" ;
+        printf "\n********** BEGIN \`%s\` **********\n" "$command" ;
         $command ;
-        printf "********** END %s **********\n\n" "$command";
+        printf "********** END \`%s\` **********\n\n" "$command";
     } >> "$OUTFILE"
 done
+
+printf "Saved data to:\n%s\n" "$OUTFILE"
